ece52fd4b5b468b82867ef54af92e7c9b0b318c8
OliverNasr <143597476+OliverNasr@users.noreply.github.com>
2024-04-15 14:23:13 +0200

    Update Transactions.h

--- Transactions.h
+++ Transactions.h
@@ -5,19 +5,17 @@
 
 using namespace std;
 
+
+
 class Transactions
 {
-	double Balance;
 	double TransferAmount;
 	string Sender, Recipient;
 public:
 	Transactions();
-	Transactions(double balance, double transferA, string reciver, string sender);
-	double getBalance();
-	~Transactions();
+	Transactions(double transferA, string reciver, string sender);
 	string getSender();
 	string getRecipient();
-	double setBalance(double newBalance);
-	double TransferMoney_As_Sender(int UserID, string reciver, string sender, double transferA);
-	double TransferMoney_As_Recipient(int UserID, string reciver, string sender, double transferA);
+	~Transactions();
 };
+
