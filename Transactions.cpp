f73a6d0b62bdb59fafc323b766fce397d69b8480
OliverNasr <143597476+OliverNasr@users.noreply.github.com>
2024-04-15 16:14:23 +0200

    Update Transactions.cpp

--- Transactions.cpp
+++ Transactions.cpp
@@ -1,29 +1,38 @@
 #include "Transactions.h"
+#include <string>
+
+
 
 Transactions::Transactions() {
 
 	TransferAmount = 0;
-	Sender = "\0";
-	Recipient = "\0";
 };
 
-Transactions::Transactions(double transferA, string reciver, string sender) {
+Transactions::Transactions(double transferA, User& sender, User* reciver) {
 
 	this->TransferAmount = transferA;
-	this->Sender = sender;
+	Sender = &sender;
 	this->Recipient = reciver;
-};
-
-
-
-string Transactions::getSender(){
-	return Sender;
 }
 
-string Transactions::getRecipient(){
-	return Recipient;
+Transactions::Transactions(double transferA, User* sender, User* reciver) {
+
+	this->TransferAmount = transferA;
+	Sender = sender;
+	this->Recipient = reciver;
 }
 
+User Transactions::getSender(){
+	return* Sender;
+}
+
+User Transactions::getRecipient(){
+	return* Recipient;
+}
+
+double Transactions::getTransferA(){
+	return TransferAmount;
+}
 
 Transactions::~Transactions() {
 
