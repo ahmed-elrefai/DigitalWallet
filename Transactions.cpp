9fce673858ff1acc33086a14324bbfe55688d093
OliverNasr <143597476+OliverNasr@users.noreply.github.com>
2024-04-15 14:22:50 +0200

    Update Transactions.cpp

--- Transactions.cpp
+++ Transactions.cpp
@@ -1,22 +1,20 @@
 #include "Transactions.h"
 
 Transactions::Transactions() {
-	Balance = 0;
+
 	TransferAmount = 0;
 	Sender = "\0";
 	Recipient = "\0";
 };
 
-Transactions::Transactions(double balance, double transferA, string reciver, string sender) {
-	this->Balance = balance;
+Transactions::Transactions(double transferA, string reciver, string sender) {
+
 	this->TransferAmount = transferA;
 	this->Sender = sender;
 	this->Recipient = reciver;
 };
 
-double Transactions::getBalance() {
-	return Balance;
-}
+
 
 string Transactions::getSender(){
 	return Sender;
@@ -26,20 +24,6 @@ string Transactions::getRecipient(){
 	return Recipient;
 }
 
-double Transactions::setBalance(double newBalance) {
-	
-	this->Balance = newBalance;
-}
-
-double Transactions::TransferMoney_As_Sender(int UserID, string reciver, string sender, double transferA) {
-
-
-}
-
-double Transactions::TransferMoney_As_Recipient(int UserID, string reciver, string sender, double transferA) {
-
-
-}
 
 Transactions::~Transactions() {
 
